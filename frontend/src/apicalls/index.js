4e25dc66905f0fcf18282ba257dc34caa7ee9476
Mundra-1997 <115582956+Mundra-1997@users.noreply.github.com>
2023-08-18 21:48:24 +0530

    Update index.js

--- frontend/src/apicalls/index.js
+++ frontend/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-     baseURL: 'http://localhost:5000',
+     baseURL: 'https://quiz-gtsj.onrender.com',
     headers: {
        'authorization': `Bearer ${localStorage.getItem('token')}`
     }
